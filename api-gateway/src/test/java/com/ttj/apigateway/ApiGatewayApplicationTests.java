3d8c8748b0e12d58e00d52eca67633e0912057e5
thetechnojournals <thetechnojournals@gmail.com>
2020-04-08 12:31:32 +0530

    adding to git.

---
+++ api-gateway/src/test/java/com/ttj/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttj.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
